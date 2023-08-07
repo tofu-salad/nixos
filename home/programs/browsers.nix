8508f4a08ffbb435da604ff418b8d78efa8c712d
zeroCalSoda <diego.ezequiel.scardulla@gmail.com>
2023-08-07 16:19:21 -0300

    added zsh

--- home/programs/browsers.nix
+++ home/programs/browsers.nix
@@ -4,8 +4,11 @@
   programs = {
     chromium = {
       enable = true;
-      commandLineArgs =
-        [ "--force-dark-mode" "--enable-features=WebUIDarkMode" ];
+      commandLineArgs = [
+        "--force-dark-mode"
+        "--enable-features=WebUIDarkMode"
+        "--enable-blink-features=MiddleClickAutoscroll"
+      ];
     };
 
     firefox = { enable = true; };
