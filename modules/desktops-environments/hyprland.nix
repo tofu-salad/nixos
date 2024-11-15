c4c73aa0b91c66b28552d664a611db67b25df7c7
tofu-salad <tofu salad>
2024-11-15 01:12:26 -0300

    automount/unmount drives

--- modules/desktops-environments/hyprland.nix
+++ modules/desktops-environments/hyprland.nix
@@ -12,12 +12,6 @@
     };
   };
 
-  services = {
-    gvfs = {
-      enable = true;
-    };
-  };
-
   environment.sessionVariables = {
     POLKIT_AUTH_AGENT = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
     GSETTINGS_SCHEMA_DIR = "${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}/glib-2.0/schemas";
