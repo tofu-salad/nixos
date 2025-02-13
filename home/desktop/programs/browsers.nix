3b7f0a65a30f699d25091e1e6670468cc3fd5cd1
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:51:00 -0300

    removed overlays + google-chrome uses spanish

--- home/desktop/programs/browsers.nix
+++ home/desktop/programs/browsers.nix
@@ -1,5 +1,9 @@
 { pkgs, config, ... }:
-
+with pkgs; let
+  patchDesktop = pkg: appName: from: to: (lib.hiPrio (runCommand "$patched-desktop-entry-for-${appName}" { } ''
+    ${coreutils}/bin/mkdir -p $out/share/applications
+    ${gnused}/bin/sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop ''));
+in
 {
   programs = {
     chromium = {
@@ -11,7 +15,8 @@
     };
 
   };
-  home.packages = with pkgs; [
+  home.packages = [
     google-chrome
+    (patchDesktop google-chrome "google-chrome" "^Exec=${google-chrome}/bin/google-chrome-stable %U" "Exec=env LANGUAGE=es_ES ${google-chrome}/bin/google-chrome-stable %U")
   ];
 }
