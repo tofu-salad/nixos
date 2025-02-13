dfa7559056b29b0887edcbca24ddc28a35ea7b01
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:51:42 -0300

    added trusted user and changed nixpkgs to nixos-24.11 instead of release

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     ];
   };
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -36,6 +36,7 @@
       };
 
       commonSettings = {
+        nix.settings.trusted-users = [ "tofu" ];
         nix.settings.experimental-features = [
           "nix-command"
           "flakes"
