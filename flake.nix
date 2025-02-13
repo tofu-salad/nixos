3efd58204be29588f4427258293bd84e4ddd4304
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:54:13 -0300

    nix-community cachix public key

--- flake.nix
+++ flake.nix
@@ -6,6 +6,9 @@
       "https://cache.nixos.org"
       "https://nix-community.cachix.org"
     ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
