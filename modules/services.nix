97143340a8dede822162ec2a5eb10de79efbf843
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:51:42 -0300

    sway refactor + using greetd+tuigreet

--- modules/services.nix
+++ modules/services.nix
@@ -5,12 +5,11 @@
   # Autologin Workaround
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
-  services.displayManager.sddm.wayland.enable = true;
   services = {
     xserver = {
       enable = true;
-      displayManager.gdm.enable = true;
-      desktopManager.gnome.enable = true;
+      displayManager.gdm.enable = false;
+      desktopManager.gnome.enable = false;
       xkb = {
         variant = "";
         layout = "us";
@@ -20,53 +19,16 @@
       enable = true;
       alsa.enable = true;
       pulse.enable = true;
-      # extraConfig.pipewire."99-input-denoising" = {
-      #   "context.modules" = [
-      #     {
-      #       name = "libpipewire-module-filter-chain";
-      #       args = {
-      #         "node.description" = "Noise Canceling source";
-      #         "media.name" = "Noise Canceling source";
-      #         "filter.graph" = {
-      #           nodes = [
-      #             {
-      #               type = "ladspa";
-      #               name = "rnnoise";
-      #               plugin = "/home/tofu/.config/pipwireBACKUP/pipewire.conf.d/librnnoise_mono.so";
-      #               label = "noise_suppressor_mono";
-      #               control = {
-      #                 "VAD Threshold (%)" = "50.0";
-      #                 "VAD Grace Period (ms)" = "200";
-      #                 "Retroactive VAD Grace (ms)" = "0";
-      #               };
-      #             }
-      #           ];
-      #         };
-      #         "capture.props" = {
-      #           "node.name" = "capture.rnnoise_source";
-      #           "node.passive" = "true";
-      #           "audio.rate" = "48000";
-      #         };
-      #         "playback.props" = {
-      #           "node.name" = "rnnoise_source";
-      #           "media.class" = "Audio/Source";
-      #           "audio.rate" = "48000";
-      #         };
-      #       };
-      #     }
-      #   ];
-      # };
     };
-    # greetd = {
-    #   enable = true;
-    #   settings = rec {
-    #     initial_session = {
-    #       command = "${pkgs.hyprland}/bin/Hyprland";
-    #       user = "tofu";
-    #     };
-    #     default_session = initial_session;
-    #   };
-    # };
+    greetd = {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd sway";
+          user = "tofu";
+        };
+      };
+    };
     dbus = {
       enable = true;
       packages = with pkgs; [ gnome-keyring ];
