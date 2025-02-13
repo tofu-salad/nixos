cb7af8f3fd6929c02538c047e904d69bae95328b
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:50:59 -0300

    added zsh

---
+++ home/programs/zsh.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+
+{
+  programs.zsh = {
+    enable = true;
+    plugins = [{
+      name = "zsh-autosuggestions";
+      src = pkgs.fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-autosuggestions";
+        rev = "v0.4.0";
+        sha256 = "0z6i9wjjklb4lvr7zjhbphibsyx51psv50gm07mbb0kj9058j6kc";
+      };
+    }];
+  };
+}
