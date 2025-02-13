490bab879f1183f4cabb9f621d6e3faae7594b1e
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-02-13 04:51:00 -0300

    kde

---
+++ modules/kde.nix
@@ -0,0 +1,18 @@
+{ inputs, pkgs, ... }:
+{
+  services.xserver.enable = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  environment.plasma5.excludePackages = with pkgs.libsForQt5; [
+    elisa
+    dolphin
+    gwenview
+    okular
+    oxygen
+    khelpcenter
+    konsole
+    plasma-browser-integration
+    print-manager
+  ];
+}
