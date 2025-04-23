000ac6aac1e04501e01407fb66587ec8acd00a0d
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-04-23 03:13:39 -0300

    samba share

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -113,7 +113,19 @@
     unzip
     vim
     wget
+    cifs-utils
   ];
+
+  fileSystems."/mnt/share" = {
+    device = "//192.168.0.12/share";
+    fsType = "cifs";
+    options =
+      let
+        automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s,user,users";
+
+      in
+      [ "${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100" ];
+  };
 
   system.stateVersion = "24.11";
 }
