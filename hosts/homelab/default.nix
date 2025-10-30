803cb3092f7dcc425ef7ed109edd6b55a0ec3e9d
tofu-salad <67925799+tofu-salad@users.noreply.github.com>
2025-10-30 04:31:38 -0300

    format

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -74,14 +74,14 @@
 
   services.gvfs.enable = true;
   services.caddy = {
-		enable = true;
-		environmentFile = "/etc/nixos/secrets/caddy.env";
-		extraConfig = ''
-    {$URL} {
-        reverse_proxy {$IP}:{$PORT}
-    }
-  '';
-	};
+    enable = true;
+    environmentFile = "/etc/nixos/secrets/caddy.env";
+    extraConfig = ''
+      {$URL} {
+          reverse_proxy {$IP}:{$PORT}
+      }
+    '';
+  };
   services.tailscale.enable = true;
   networking.nftables.enable = true;
   networking.firewall = {
